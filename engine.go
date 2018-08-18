98938abc4a353db8ed415e0464c8afc7a9fc1c3d
Erik Gilling <konkers@google.com>
2018-08-18 12:01:13 -0700

    Initial working version.

---
+++ engine.go
@@ -0,0 +1,74 @@
+package cmd
+
+import (
+	"fmt"
+
+	shellwords "github.com/mattn/go-shellwords"
+)
+
+// Handler is a function that gets invoked when a command is run.
+type Handler func(ctx interface{}, args []string) error
+
+type commandInfo struct {
+	handler   Handler
+	userLevel int
+	help      string
+}
+
+// Engine is the core interface to the command interpreter.
+type Engine struct {
+	commands map[string]*commandInfo
+	parser   *shellwords.Parser
+}
+
+// NewEngine creates a new command interpreter.
+func NewEngine() *Engine {
+	return &Engine{
+		commands: make(map[string]*commandInfo),
+		parser:   shellwords.NewParser(),
+	}
+}
+
+// AddCommand adds a command to the engine.
+func (e *Engine) AddCommand(name string, help string, handler Handler, userLevel int) error {
+	if _, ok := e.commands[name]; ok {
+		return fmt.Errorf("command \"%s\" already registered", name)
+	}
+
+	e.commands[name] = &commandInfo{
+		handler:   handler,
+		userLevel: userLevel,
+		help:      help,
+	}
+
+	return nil
+}
+
+// Exec executes a command.
+func (e *Engine) Exec(ctx interface{}, userLevel int, args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("command args need to be > 0")
+	}
+
+	cmd, ok := e.commands[args[0]]
+
+	if !ok {
+		return fmt.Errorf("command \"%s\" not found", args[0])
+	}
+
+	if cmd.userLevel > userLevel {
+		return fmt.Errorf("user level %d not >= %d", userLevel, cmd.userLevel)
+	}
+
+	return cmd.handler(ctx, args[1:])
+}
+
+// ExecString parses the commandString and executes the command.
+func (e *Engine) ExecString(ctx interface{}, userLevel int, commandString string) error {
+	args, err := e.parser.Parse(commandString)
+	if err != nil {
+		return err
+	}
+
+	return e.Exec(ctx, userLevel, args)
+}
---
+++ engine_test.go
@@ -0,0 +1,137 @@
+package cmd
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestNewEngine(t *testing.T) {
+	e := NewEngine()
+	if e == nil {
+		t.Errorf("NewEngine() returned nil")
+	}
+}
+
+func TestAddHandler(t *testing.T) {
+	e := NewEngine()
+	h := func(ctx interface{}, args []string) error { return nil }
+
+	err := e.AddCommand("test", "help", h, 0)
+	if err != nil {
+		t.Errorf("AddCommand() failed")
+	}
+
+	err = e.AddCommand("test", "help", h, 0)
+	if err == nil {
+		t.Errorf("Duplicate AddCommand() succeeded")
+	}
+}
+
+func TestExecHandler(t *testing.T) {
+	var handlerArgs []string
+	var err error
+	h := func(ctx interface{}, args []string) error {
+		handlerArgs = args
+		if len(args) >= 1 && args[0] == "bad" {
+			return fmt.Errorf("bad")
+		} else {
+			return nil
+		}
+	}
+
+	e := NewEngine()
+	e.AddCommand("test", "help", h, 0)
+
+	type testdata struct {
+		ctx           interface{}
+		userLevel     int
+		args          []string
+		errorExpected bool
+		argsExpected  bool
+	}
+	tests := []testdata{
+		{nil, 0, []string{}, true, false},               // 0
+		{nil, 0, []string{"nope"}, true, false},         // 1
+		{nil, -1, []string{"test"}, true, false},        // 2
+		{nil, 0, []string{"test"}, false, true},         // 3
+		{nil, 0, []string{"test", "good"}, false, true}, // 4
+		{nil, 0, []string{"test", "bad"}, true, true},   // 5
+	}
+
+	for i, test := range tests {
+		handlerArgs = nil
+		err = e.Exec(test.ctx, test.userLevel, test.args)
+		if test.errorExpected && err == nil {
+			t.Errorf("%d: Expected error but none was returned.", i)
+		} else if !test.errorExpected && err != nil {
+			t.Errorf("%d: Expected no error but one was returned.", i)
+		}
+
+		if test.argsExpected {
+			if !reflect.DeepEqual(handlerArgs, test.args[1:]) {
+				t.Errorf("%d: handlerArgs(%#v) != exepected(%#v)", i, handlerArgs, test.args[1:])
+			}
+		} else {
+			if handlerArgs != nil {
+				t.Errorf("%d: expected handlerArgs be nil, instead they are %#v",
+					i, handlerArgs)
+			}
+		}
+	}
+}
+
+func TestExecString(t *testing.T) {
+	var handlerArgs []string
+	var err error
+	h := func(ctx interface{}, args []string) error {
+		handlerArgs = args
+		if len(args) >= 1 && args[0] == "bad" {
+			return fmt.Errorf("bad")
+		} else {
+			return nil
+		}
+	}
+
+	e := NewEngine()
+	e.AddCommand("test", "help", h, 0)
+
+	type testdata struct {
+		ctx           interface{}
+		userLevel     int
+		args          string
+		errorExpected bool
+		expectedArgs  []string
+	}
+	tests := []testdata{
+		{nil, 0, "\"", true, nil},                      // 0
+		{nil, 0, "", true, nil},                        // 1
+		{nil, 0, "nope", true, nil},                    // 2
+		{nil, -1, "test", true, nil},                   // 3
+		{nil, 0, "test", false, []string{}},            // 4
+		{nil, 0, "test good", false, []string{"good"}}, // 5
+		{nil, 0, "test bad", true, []string{"bad"}},    // 6
+	}
+
+	for i, test := range tests {
+		handlerArgs = nil
+		err = e.ExecString(test.ctx, test.userLevel, test.args)
+		if test.errorExpected && err == nil {
+			t.Errorf("%d: Expected error but none was returned.", i)
+		} else if !test.errorExpected && err != nil {
+			t.Errorf("%d: Expected no error but one was returned.", i)
+		}
+
+		if test.expectedArgs != nil {
+			if !reflect.DeepEqual(handlerArgs, test.expectedArgs) {
+				t.Errorf("%d: handlerArgs(%#v) != exepected(%#v)",
+					i, handlerArgs, test.expectedArgs)
+			}
+		} else {
+			if handlerArgs != nil {
+				t.Errorf("%d: expected handlerArgs be nil, instead they are %#v",
+					i, handlerArgs)
+			}
+		}
+	}
+}
